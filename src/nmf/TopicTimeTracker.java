823989b31ecd1593e2f7e0e18a5d1d6bf003df05
Daniel <daniel.brueggemann.mail@gmail.com>
2016-06-09 16:32:32 +0200

    Fixed Visualization.

--- src/nmf/TopicTimeTracker.java
+++ src/nmf/TopicTimeTracker.java
@@ -5,7 +5,7 @@ import model.TopicRiver;
 
 public class TopicTimeTracker {
 	public static void main(String[] args) {
-		TopicRiver tr = TopicRiver.loadTopicRiver("/media/Storage/Meine Daten/Schutzbereich/MoS/Research Project 2/TopicMiningProject/ressources/year Tue Aug 20 00:00:00 CEST 1996");
+		TopicRiver tr = TopicRiver.loadTopicRiver("/media/Storage/Meine Daten/Schutzbereich/MoS/Research Project 2/savedata/temp");
 		System.out.println(tr);
     
     VisualizationDataConverter.writeJSONData(tr);
--- src/nmf/VisualizationDataConverter.java
+++ src/nmf/VisualizationDataConverter.java
@@ -39,64 +39,49 @@ public class VisualizationDataConverter
 
     for (TopicWave wave : waves)
     {
-//      List<Double> yValues = new ArrayList<>();
-//      TreeMap<Integer, Topic> topicSequence = wave.getTopicSequence();
+      List<Double> yValues = new ArrayList<>();
+      TreeMap<Integer, Topic> topicSequence = wave.getTopicSequence();
 
-//      HashMap<String, Double> bestTermMap = new HashMap<>();
+      HashMap<String, Double> bestTermMap = new HashMap<>();
+
+      for (int i = 1; i <= timeSteps; i++)
+      {
+        Topic topic = topicSequence.get(i);
+        double relativeRelevance;
+        if (topic == null)
+        {
+          relativeRelevance = 0;
+        }
+        else
+        {
+          relativeRelevance = topic.getRelativeRelevance();
+
+          HashMap<String, Double> localBestTermMap = topic.getBestTerms(5);
+          for (Entry<String, Double> entry : localBestTermMap.entrySet())
+          {
+            Double existingValue = bestTermMap.get(entry.getKey());
+            Double entryValue = entry.getValue();
+            if (existingValue != null)
+            {
+              entryValue += existingValue;
+            }
+            bestTermMap.put(entry.getKey(), entryValue);
+          }
+        }
+        yValues.add(relativeRelevance);
+      }
+      yValueMap.put(wave, yValues);
 //
-//      for (int i = 1; i <= timeSteps; i++)
-//      {
-//        Topic topic = topicSequence.get(i);
-//        double relativeRelevance;
-//        if (topic == null)
-//        {
-//          relativeRelevance = 0;
-//        }
-//        else
-//        {
-//          relativeRelevance = topic.getRelativeRelevance(topicRiver.
-//                  getTfidfTotal(i));
-//
-//          HashMap<String, Double> localBestTermMap = topic.getBestTerms(5);
-//          for (Entry<String, Double> entry : localBestTermMap.entrySet())
-//          {
-//            Double existingValue = bestTermMap.get(entry.getKey());
-//            Double entryValue = entry.getValue();
-//            if (existingValue != null)
-//            {
-//              entryValue += existingValue;
-//            }
-//            bestTermMap.put(entry.getKey(), entryValue);
-//          }
-//        }
-//        yValues.add(relativeRelevance);
-//      }
-//      yValueMap.put(wave, yValues);
-//
-//      List<Map.Entry<String, Double>> entryList = new LinkedList<>(
-//              bestTermMap.entrySet());
-//      Collections.sort(entryList, new Comparator<Entry<String, Double>>()
-//      {
-//        @Override
-//        public int compare(Entry<String, Double> o1, Entry<String, Double> o2)
-//        {
-//          return (int) Math.signum(o2.getValue() - o1.getValue());
-//        }
-//      });
-//      int max = Math.min(entryList.size(), 5);
-//      List<String> bestTerms = new ArrayList<String>();
-//      for (int i = 0; i < max; i++)
-//      {
-//        String term = entryList.get(i).getKey();
-//        bestTerms.add(term);
-//      }
       int termCount = 0;
-      List<String> bestTerms = new ArrayList<String>();
-      for(Entry<Double,String> terms: wave.getAverageTFIDF().entrySet()){
-    	  bestTerms.add(terms.getValue());
-    	  termCount++;
-    	  if(termCount==5)
-    		  break;
+      List<String> bestTerms = new ArrayList<>();
+      for (Entry<Double, String> terms : wave.getAverageTFIDF().descendingMap().entrySet())
+      {
+        bestTerms.add(terms.getValue());
+        termCount++;
+        if (termCount == 5)
+        {
+          break;
+        }
       }
       String name = String.join(", ", bestTerms);
       nameMap.put(wave, name);
