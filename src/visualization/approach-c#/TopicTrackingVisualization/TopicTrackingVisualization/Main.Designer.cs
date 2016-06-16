4f13e11f63e1a89d30bfe7d074c3109d60459d39
Darius <darius.schneider@gmx.de>
2016-06-16 11:30:43 +0200

    Added Visualization and Webcrawler

---
+++ src/visualization/approach-c#/TopicTrackingVisualization/TopicTrackingVisualization/Main.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace TopicTrackingVisualization
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+			this.topicOverviewChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.randomToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.tESTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			((System.ComponentModel.ISupportInitialize)(this.topicOverviewChart)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// topicOverviewChart
+			// 
+			chartArea1.Name = "ChartArea1";
+			this.topicOverviewChart.ChartAreas.Add(chartArea1);
+			this.topicOverviewChart.Location = new System.Drawing.Point(12, 27);
+			this.topicOverviewChart.Name = "topicOverviewChart";
+			this.topicOverviewChart.Size = new System.Drawing.Size(533, 361);
+			this.topicOverviewChart.TabIndex = 0;
+			this.topicOverviewChart.Text = "chart1";
+			this.topicOverviewChart.AxisViewChanged += new System.EventHandler<System.Windows.Forms.DataVisualization.Charting.ViewEventArgs>(this.topicOverviewChart_AxisViewChanged);
+			this.topicOverviewChart.PostPaint += new System.EventHandler<System.Windows.Forms.DataVisualization.Charting.ChartPaintEventArgs>(this.topicOverviewChart_PostPaint);
+			this.topicOverviewChart.Paint += new System.Windows.Forms.PaintEventHandler(this.topicOverviewChart_Paint);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(557, 24);
+			this.menuStrip1.TabIndex = 1;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// fileToolStripMenuItem
+			// 
+			this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.randomToolStripMenuItem,
+            this.tESTToolStripMenuItem});
+			this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+			this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+			this.fileToolStripMenuItem.Text = "File";
+			// 
+			// openToolStripMenuItem
+			// 
+			this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+			this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+			this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.openToolStripMenuItem.Text = "Open";
+			this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+			// 
+			// randomToolStripMenuItem
+			// 
+			this.randomToolStripMenuItem.Name = "randomToolStripMenuItem";
+			this.randomToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.randomToolStripMenuItem.Text = "Random";
+			this.randomToolStripMenuItem.Click += new System.EventHandler(this.randomToolStripMenuItem_Click);
+			// 
+			// tESTToolStripMenuItem
+			// 
+			this.tESTToolStripMenuItem.Name = "tESTToolStripMenuItem";
+			this.tESTToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+			this.tESTToolStripMenuItem.Text = "TEST";
+			this.tESTToolStripMenuItem.Click += new System.EventHandler(this.tESTToolStripMenuItem_Click);
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(557, 402);
+			this.Controls.Add(this.topicOverviewChart);
+			this.Controls.Add(this.menuStrip1);
+			this.Name = "Main";
+			this.Text = "Topic Tracking";
+			this.Resize += new System.EventHandler(this.Main_Resize);
+			((System.ComponentModel.ISupportInitialize)(this.topicOverviewChart)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataVisualization.Charting.Chart topicOverviewChart;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem randomToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem tESTToolStripMenuItem;
+	}
+}
---
+++ src/webcrawler/Reuters Archive Crawler/ReutersCrawler/ReutersCrawler/CLogEntry.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReutersCrawler
+{
+	public enum ELogcode { Status, Output, Warning, Error };
+
+	class CLogEntry
+	{
+		private DateTime _time;
+		private ELogcode _logcode;
+		private string _message;
+
+		public DateTime Time { get { return _time; } }
+		public ELogcode Logcode { get { return _logcode; } }
+		public string Message { get { return _message; } }
+
+		public CLogEntry(ELogcode logcode, string message)
+		{
+			_time = DateTime.Now;
+			_logcode = logcode;
+			_message = message;
+		}
+
+		public override string ToString()
+		{
+			return _time.ToString("s") + " - " + _logcode.ToString() + " - " + _message;
+		}
+	}
+}
